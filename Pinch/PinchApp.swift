0777f013a89a465901be834541dbf8e2b121fd32
Daniel Lopes <daniel.dxdb@gmail.com>
2022-05-19 19:33:24 -0300

    First commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Daniel Lopes on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
